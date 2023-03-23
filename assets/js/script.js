fe92276e3b18827d52d19c42e116ae37f8d0a8e4
muslim_top <128486977+muslim-top@users.noreply.github.com>
2023-03-23 01:37:30 -0700

    Update script.js
    to let the Hijri date updatable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-let currentIslamicDate = new Date(2023, 2, 21); // Note: month is 0-based index, so March is 2
-let formattedIslamicDate = currentIslamicDate.toLocaleDateString('ar-SA', {
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric',
-  calendar: 'islamic'
-});
-document.getElementById("current-islamic-date").innerHTML = formattedIslamicDate;
+// for Hijri date
+let currentIslamicDate = new Date(); // Gets the current date and time
+    let formattedIslamicDate = currentIslamicDate.toLocaleDateString('ar-SA', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      calendar: 'islamic'
+    });
+    document.getElementById("current-islamic-date").innerHTML = formattedIslamicDate;
+// /Hijri date
